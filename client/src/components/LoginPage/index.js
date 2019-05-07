f92e0cb82a7c2a6b1de597c23b66f74043bc3b30
Keenan Pautler <kpautler@henselphelps.com>
2019-05-07 14:02:00 -0600

    Redesign Login Page

--- client/src/components/LoginPage/index.js
+++ client/src/components/LoginPage/index.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import axios from 'axios';
+import backgroundImage from '../../assets/pictures/loginBackground3.jpg';
 
 const LOCALSTORAGE_ACCESS_TOKEN_KEY = 'spotify-token';
 const LOCALSTORAGE_ACCESS_TOKEN_EXPIRY_KEY = 'spotify-token-expires-in';
+
+const styles = {
+  loginBackground: {
+    backgroundImage: `url(${backgroundImage})`,
+    position: 'absolute',
+    backgoundRepeat: 'no-repeat',
+    display: 'flex',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    backgroundPosition: '50%',
+    flexWrap: 'wrap',
+    backgroundSize: 'cover',
+    width: '100vw',
+    height: '100vh'
+  },
+  loginButton: {
+    display: 'block',
+    padding: '1em 3em .98em',
+    textAlign: 'center',
+    margin: '0 auto',
+    width: 'auto'
+  },
+  loginContent: {
+    maxWidth: '1025px',
+    margin: '0 auto',
+    position: 'relative'
+  },
+  loginTitle: {
+    color: 'white',
+    margin: '0 auto',
+    marginTop: 100,
+    textAlign: 'center',
+    fontSize: 64
+  },
+  loginDescription: {
+    fontSize: 36,
+    color: 'white',
+    margin: '5vh 0 40vh',
+    textAlign: 'center'
+  }
+};
 
 class LoginPage extends Component {
   state = {
@@ -34,7 +77,6 @@ class LoginPage extends Component {
           return initial;
         }, {});
     }
-    console.log('hello');
     if (
       localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_KEY) &&
       parseInt(
@@ -62,7 +104,21 @@ class LoginPage extends Component {
   render() {
     return (
       <div>
-        <Button onClick={this.getRedirectUri}> Log Into Spotify </Button>
+        <div style={styles.loginBackground} />
+        <div style={styles.loginContent}>
+          <h1 style={styles.loginTitle}> Searchify </h1>
+          <h3 style={styles.loginDescription}>
+            An App for finding less popular songs by genre using the Spotify Web
+            API
+          </h3>
+          <Button
+            style={styles.loginButton}
+            variant="contained"
+            onClick={this.getRedirectUri}
+            size="large">
+            Login to Spotify
+          </Button>
+        </div>
       </div>
     );
   }
